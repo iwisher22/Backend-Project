eaa3c54dc67a3d0ed82deb7aaf57047a7dafcac5
iwisher22 <ashusingh9559299029@gmail.com>
2023-12-25 00:50:44 +0530

    middlewares

---
+++ src/utils/cloudnary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDNARY_CLOUD_NAME,
+  api_key: process.env.CLOUDNARY_API_KEY,
+  api_secret: process.env.CLOUDNARY_API_SECRET,
+});
+
+const uploadOnCloudnary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    console.log("File is uploaded in cloudnary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temprory file as the operation got failed
+    return null;
+  }
+};
+
+export { uploadOnCloudnary };
