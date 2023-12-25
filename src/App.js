b379d2a48b249611d51ad9d78511eebdcd75228c
iwisher22 <ashusingh9559299029@gmail.com>
2023-12-25 22:53:11 +0530

    controller

--- src/App.js
+++ src/App.js
@@ -16,4 +16,8 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express("public"));
 app.use(express(cookieParser()));
 
+import userRouter from "./routes/user.routes.js";
+
+app.use("/users", userRouter);
+
 export default app;
