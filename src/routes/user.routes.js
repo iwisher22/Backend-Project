56b9b4d0a3e5cab1466890cd5f76a6107d01b300
iwisher22 <ashusingh9559299029@gmail.com>
2024-01-08 00:09:57 +0530

    models

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -13,6 +13,7 @@ import {
   getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
